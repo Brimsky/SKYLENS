2eb0891ec8ac1112d5e37a1af0cace26c258fcab
Brimsky <juruma66@gmail.com>
2024-07-15 02:45:48 +0300

    home page user auth dropdown

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -3,7 +3,7 @@ import { Link } from '@inertiajs/vue3';
 import { computed } from 'vue';
 
 import logo from '../icons/skylens.png';
-
+import userlight from '../icons/user/user-light.png';
 // import { useDark, useToggle} from '@vueuse/core';
   
 // import lightIcon from '../icons/Logo/icon.png';
@@ -41,7 +41,10 @@ defineProps({
 
 import { ref } from 'vue';
 
-const open = ref(false);
+
+import Dropdown from '@/Components/Dropdown.vue';
+import DropdownLink from '@/Components/DropdownLink.vue';
+const open = ref(false)
 </script>
 
 <template class="antialiased">
@@ -56,25 +59,24 @@ const open = ref(false);
         </div>
 
         <div>
-          
           <div class="md:hidden">
-
-    <button @click="open = !open" class="p-4 focus:outline-none">
-      <svg v-show="!open" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7"/>
-      </svg>
-      <svg v-show="open" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/>
-      </svg>
-    </button>
-
-
-    <div v-show="open" class="absolute top-0 left-0 w-full bg-neutral-900 text-white z-50">
-      <div class="flex items-center justify-between px-4 py-3 border-b border-gray-700">
-        <a href="/" class="text-xl font-semibold">SKYLENS</a>
-        <button @click="open = false" class="text-gray-400 focus:outline-none">
-          
-        </button>
+            <button @click="open = !open" class="p-4 focus:outline-none">
+              <svg v-show="!open" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7"/>
+              </svg>
+              <svg v-show="open" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/>
+              </svg>
+            </button>
+            
+          <div v-show="open" class="absolute top-0 left-0 w-full bg-neutral-900 text-white z-50">
+            <div class="flex items-center justify-between px-4 py-3 border-b border-gray-700">
+              <a href="/" class="text-xl font-semibold">SKYLENS</a>
+              <button @click="open = false" class="text-gray-400 focus:outline-none">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/>
+                </svg>
+              </button>
       </div>
 
       <div class="px-2 py-2 space-y-1">
@@ -94,7 +96,7 @@ const open = ref(false);
   </div>
 
   </div>
-      <div v-if="canLogin" class="items-center space-x-6  hidden md:flex">
+      <!-- <div v-if="canLogin" class="items-center space-x-6  hidden md:flex">
         <template v-if="$page.props.auth.user">
           <Link :href="route('dashboard')"
                 class="navbar-btn">
@@ -114,12 +116,31 @@ const open = ref(false);
                 class="navbar-btn">
             Register
           </Link>
-          <button @click="toggleDark()"
+          button @click="toggleDark()"
                   class="py-2 px-4 bg-stone-900 hover:bg-stone-700 dark:hover:bg-stone-400 dark:bg-white  text-white font-bold rounded-xl shadow-md transition duration-150">
             <img class="h-7" :src="mode" alt="Dark Mode Icon" />
           </button>
         </template>
-      </div>
+      </div> -->
+      <div class="ms-3 relative">
+  <Dropdown align="right" width="48">
+    <template #trigger>
+      <span class="inline-flex rounded-md">
+        <button
+        type="button"
+        class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500  hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
+        >
+        <img class="scale-50" :src="userlight" alt="Icon" />
+    </button>
+  </span>
+</template>
+<template #content>
+  <DropdownLink :href="route('login')"> LOG IN </DropdownLink>
+  <DropdownLink :href="route('register')"> SIGN UP </DropdownLink>
+</template>
+</Dropdown>
+</div>
+
     </div>
 </div>
 
